1542d90ec6a098c1b4c0164e01c2836b60cfc5b5
Zero <zeroa5620@gmail.com>
2018-11-25 18:00:54 +0545

    script for installing all dependent libraries added

---
+++ install.sh
@@ -0,0 +1,7 @@
+npm install bufferutil
+npm install hammerandchisel/erlpack
+npm install sodium
+npm install libsodium-wrappers
+npm install uws
+npm install discord.js node-opus
+npm install discord.js opusscript
