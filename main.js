3bc2c7c3249b9f3fdafc01b8dca78550e5722cdd
Zero <zeroa5620@gmail.com>
2018-11-25 19:32:03 +0545

    Welcome message for new users added

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ const BOT = new DISCORD.Client();
 const FS = require('fs');
 const STRUCT = require('c-struct');
 
-
 // Globals
 var DiscordParms = new STRUCT.Schema(
-    { 
-        token: STRUCT.type.string(), 
-        prefix: STRUCT.type.string(1),
-        ready: STRUCT.type.u8(0) 
+    {
+        token: STRUCT.type.string(),
+        prefix: STRUCT.type.string(),
+        ready: STRUCT.type.u8(0)
     }
 );
 
@@ -28,27 +27,47 @@ if (process.platform === "win32") {
         output: process.stdout
     });
 
-    rl.on("SIGINT", function() {
+    rl.on("SIGINT", function () {
         process.emit("SIGINT");
     });
 }
-process.on("SIGINT", function() {
+process.on("SIGINT", function () {
     console.log("Disconnecting bot..")
-    
+
     process.exit();
 });
 
 // Bot connection
 BOT.on('ready', () => {
+    if (DiscordParms.ready == 1) {
+        console.log("Reconnected!");
+        return;
+    }
+
     console.log(`Logged in as ${BOT.user.tag}!`);
 
     // Bot is ready
     DiscordParms.ready = 1;
 });
 
+
+// When new member arrives send this message
+BOT.on('guildAddMember', member => {
+    member.send('Welcome to the server,' + `${member}` + 
+    '. Please goto #welcome channel for more info on the server.' +
+    'Also you can call me any time with the command `!help` from #bot in the server! Enjoy!!');
+});
+
 BOT.on('message', Msg => {
     // Don't process the message if it is from bot
-    if(Msg.author.bot)
+    if (Msg.author.bot)
+        return;
+
+    console.log(Msg.content);
+    console.log(DiscordParms.prefix);
+    console.log(Msg.content.startsWith(DiscordParms.prefix));
+
+    if (!Msg.content.startsWith(DiscordParms.prefix))
         return;
 
     if (Msg.content === DiscordParms.prefix + 'ping') {
