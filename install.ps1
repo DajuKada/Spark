57c494066b1e184233f53cd191575cae25fc2d36
Zero <zeroa5620@gmail.com>
2018-11-25 18:41:45 +0545

    Installer file added for windows

---
+++ install.ps1
@@ -0,0 +1,9 @@
+npm install fs
+npm install c-struct --save
+npm install bufferutil
+npm install hammerandchisel/erlpack
+npm install sodium
+npm install libsodium-wrappers
+npm install uws
+npm install discord.js node-opus
+npm install discord.js opusscript
